da9e39a0b3dcf8d20bcd56fce27ca4de1223de8c
tejas <54097365+tejxv@users.noreply.github.com>
2021-08-15 23:17:58 +0530

    copy pasted modal code

--- js/new.js
+++ js/new.js
@@ -18260,3 +18260,42 @@ const getAnimationTime = () => {
 
 randomizeText();
 setInterval(randomizeText, getAnimationTime());
+
+// MODAL LOGIC
+
+function modalLoading(){
+    console.log('loading')
+}
+
+function onLoadfun(){
+var modal = document.getElementById("myModal");
+
+
+var span = document.getElementsByClassName("close")[0];
+ const buttonModal = document.getElementById("modal-button");
+ console.log('run');
+ setTimeout(()=>{
+     modal.style.display = "block";
+ }, 1000);
+
+
+span.onclick = function() {
+ modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+ if (event.target == modal) {
+   modal.style.display = "none";
+ }
+}
+}
+
+function closemodal(){
+var modal = document.getElementById("myModal");
+modal.style.display ="none"
+
+setTimeout(()=>{
+    modal.style.display ="none"
+},500);
+swal("Thank You!", "We have Registered Your Request! We Will Revert you back soon", "success");
+}
